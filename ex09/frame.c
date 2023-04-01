40bdd9e94778c41cbf2b25c92a7b36ab6807634c
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2023-04-01 22:20:16 +0200

    9 is done

--- ex09/frame.c
+++ ex09/frame.c
@@ -2,39 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-int	main(int argc, char **argv)
+void	ft_draw_body(char *input, int size)
 {
 	int		count;
-	int		len;
-	int		word_len;
-	char	*input;
 	char	*str;
 
-	if (argc == 1)
-		return (1);
-	count = -1;
-	input = calloc(strlen(argv[1]), sizeof(char));
-	strcpy(input, argv[1]);
 	str = strtok(input, " ");
-	len = strlen(str);
 	while (str)
 	{
-		str = strtok(NULL, " ");
-		if (str && strlen(str) > len)
-			len = strlen(str);
-	}
-	len += 4;
-	while (++count < len)
-		printf("*");
-	printf("\n");
-	str = strtok(argv[1], " ");
-	while (str)
-	{
-		word_len = strlen(str);
 		count = -1;
-		while (++count <= len)
+		while (++count <= size)
 		{
-			if (!count || count == len)
+			if (!count || count == size)
 				printf("*");
 			else if (count == 2)
 				count += printf("%s", str);
@@ -45,8 +24,35 @@ int	main(int argc, char **argv)
 		str = strtok(NULL, " ");
 	}
 	count = -1;
+	while (++count < size)
+		printf("*");
+	printf("\n");
+}
+
+int	main(int argc, char **argv)
+{
+	int		count;
+	int		len;
+	char	*input;
+	char	*str;
+
+	if (argc == 1)
+		return (1);
+	input = calloc(strlen(argv[1]) + 1, sizeof(char));
+	strcpy(input, argv[1]);
+	str = strtok(argv[1], " ");
+	len = strlen(str);
+	while (str)
+	{
+		str = strtok(NULL, " ");
+		if (str && strlen(str) > len)
+			len = strlen(str);
+	}
+	len += 4;
+	count = -1;
 	while (++count < len)
 		printf("*");
 	printf("\n");
+	ft_draw_body(input, len);
 	free(input);
 }
