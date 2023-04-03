848ac9ce27f85e462ed7f2a0cbcf826134864e8e
Mohammed Elkholy <28461793+SolarisCode@users.noreply.github.com>
2023-04-04 01:17:24 +0200

    12 is done with pdf as an attchment

--- ex12/smpt_email.py
+++ ex12/smpt_email.py
@@ -1,15 +1,13 @@
 import os
-import ssl
 import smtplib
-from getpass import getpass
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 
 msg = MIMEMultipart()
-from_addr = input("Enter your gmail: ")
-password = getpass("Enter your password: ")
+from_addr = "msearnold@gmail.com"
+password = "iibuoowezaictitb"
 to_addr = input("Enter the receiver email: ")
 subject = input("Enter the subject: ")
 message = input("Write the content of the message then press enter:\n")
@@ -22,13 +20,12 @@ msg["Subject"] = subject
 msg.attach(MIMEText(message, "plain"))
 attachment = open(file, "rb")
 
-part = MIMEBase("application", "octet-stream")
+part = MIMEBase("application", "pdf")
 part.set_payload(attachment.read())
 encoders.encode_base64(part)
-part.add_header(file_des, f"attachment; file {os.path.split(file)[-1]}")
+part.add_header(file_des, "attachment", filename=os.path.split(file)[-1])
 msg.attach(part)
 
-# ssl_context = ssl.create_default_context()
 smtp_server = "smtp.gmail.com:587"
 server = smtplib.SMTP(smtp_server)
 server.ehlo()
@@ -36,9 +33,4 @@ server.starttls()
 server.login(from_addr, password)
 server.sendmail(from_addr, to_addr, msg.as_string())
 server.quit()
-# with smtplib.SMTP_SSL(smtp_server, 465, context=ssl_context) as server:
-#     server.login(from_addr, password)
-#     server.sendmail(from_addr, to_addr, msg.as_string())
-#     server.quit()
 attachment.close()
-
