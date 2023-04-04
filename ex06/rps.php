03b35a6367e5c1e8c906d96b2dbec8d376a02804
melkholy <28461793+SolarisCode@users.noreply.github.com>
2023-04-04 07:01:55 +0200

    6 is done

---
+++ ex06/rps.php
@@ -0,0 +1,50 @@
+<?php
+$user = strtolower(readline("Choose rock, paper, or scissors: "));
+$num = rand(1,100);
+$choices = array("rock", "paper", "scissors");
+if (!(in_array($user, $choices)))
+{
+	echo "Invalid input, Please try again.\n";
+	exit();
+}
+$comp = $choices[$num % 3];
+
+if (strcmp($user, $comp) == 0)
+{
+	echo "Sorry, you and the Computer chose ", $user, ", try again.\n"; 
+}
+else
+{
+	switch ($user) {
+		case "rock":
+			if (strcmp($comp, "paper") == 0)
+			{
+				echo "Sorry, you lost. The computer chose paper.\n";
+			}
+			else
+			{
+				echo "Congratulations! You won! The computer chose scissors.\n";
+			}
+			break ;
+		case "paper":
+			if (strcmp($comp, "scissors") == 0)
+			{
+				echo "Sorry, you lost. The computer chose scissors.\n";
+			}
+			else
+			{
+				echo "Congratulations! You won! The computer chose rock.\n";
+			}
+			break ;
+		default:
+			if (strcmp($comp, "rock") == 0)
+			{
+				echo "Sorry, you lost. The computer chose rock.\n";
+			}
+			else
+			{
+				echo "Congratulations! You won! The computer chose paper.\n";
+			}
+	}
+}
+?>
